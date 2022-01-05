7c7b4532ffb7461f0a7bfda1368eaff85eb40f9b
buffelsafari <buffelsafari@hotmail.com>
2022-01-05 12:03:08 +0100

    started a testproject

---
+++ TestProject/ProductComponents.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using Bunit;
+using BlazorServerCrud1.Components.ProductComponents;
+
+
+namespace Test.ProductComponents
+{
+    public class ProductComponents:TestContext
+    {
+
+        [Fact]
+        public void Test1()
+        {
+            
+
+            var cut = RenderComponent<ProductTitle>(parameters=>parameters.AddChildContent("hello world"));
+            
+
+            
+            cut.Find("div").MarkupMatches("<div>hello world</div>");
+
+
+        }
+
+
+
+    
+    }
+
+
+}
