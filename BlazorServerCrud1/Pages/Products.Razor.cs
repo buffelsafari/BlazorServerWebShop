474c5be01d030aa0ecedf95b7b5af8e5de803919
buffelsafari <buffelsafari@hotmail.com>
2022-01-06 16:41:29 +0100

    some paginator changes

--- BlazorServerCrud1/Pages/Products.Razor.cs
+++ BlazorServerCrud1/Pages/Products.Razor.cs
@@ -2,6 +2,7 @@
 using CosmosDBData;
 using CosmosDBService.DTO.Product;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 
@@ -12,28 +13,21 @@ namespace BlazorServerCrud1.Pages
 
     public partial class Products : ComponentBase, IDisposable
     {
-        //[CascadingParameter]
-        //public PaginatorParam? PaginatorCascade { get; set; }
-
+        
         [CascadingParameter]
         public string? SearchWord { get; set; }
 
-
-
-        [CascadingParameter]
-        public Action<int, int>? PaginatorCallback { get; set; }
-
-        [CascadingParameter(Name = "ItemsPerPage")]
-        public int ItemsPerPage { get; set; }
-
-        [CascadingParameter(Name = "SelectedPage")]
-        public int SelectedPage { get; set; }
-
-
-
         
+        private int itemsPerPage=10;        
+        private int selectedPage = 0;        
+        private int totalNumberOfItems;
+        private string totalPath="";
 
 
+        [Parameter]
+        [SupplyParameterFromQuery(Name ="id")]
+        public int? Id { get; set; } = null;
+        
 
         [Parameter]
         public string? Path { get; set; }
@@ -48,16 +42,18 @@ namespace BlazorServerCrud1.Pages
         public string? SubPath3 { get; set; }
 
 
+
+
         [Parameter]
         public List<ProductCardDTO> Items { get; set; } =new List<ProductCardDTO>();
 
         [Parameter]
-        public IAsyncEnumerable<ProductCardDTO> ItemsAsync { get; set; }
+        public IAsyncEnumerable<ProductCardDTO>? ItemsAsync { get; set; }
 
 
-        private string? _path;
+        
 
-        private CancellationTokenSource tokenSource;
+        private CancellationTokenSource? tokenSource;
 
 
         protected override Task OnInitializedAsync()
@@ -65,9 +61,9 @@ namespace BlazorServerCrud1.Pages
             
 
             //SelectedPage = 0;
-            _path = Path;
+            //_path = Path;
 
-            
+
 
             return base.OnInitializedAsync();
 
@@ -77,52 +73,59 @@ namespace BlazorServerCrud1.Pages
         protected async override Task OnParametersSetAsync()
         {
             
-            tokenSource?.Cancel();
-            tokenSource = new CancellationTokenSource();
 
 
-            if (Path != _path)  // todo detect search changes
-            {
-                SelectedPage = 0;
-                Debug.WriteLine("Type change detected");
-                _path = Path;
-                //StateHasChanged();
-                
-            }
+            Debug.WriteLine("on parameter set");
 
-            
-            //await GetStuff();
+            totalPath = Path != null ? Path + "/" : "";
+            totalPath += SubPath1 != null ? SubPath1 + "/" : "";
+            totalPath += SubPath2 != null ? SubPath2 + "/" : "";
+            totalPath += SubPath3 != null ? SubPath3 + "/" : "";
 
-            // todo only db when changes detected
-            //TotalCount = await cosmosDB.ValueSQLCount(Type != null ? Type : "", SearchWord != null ? SearchWord : "");            
 
-            string typeCombo = Path != null ? Path + "/" : "";
-            typeCombo += SubPath1 != null ? SubPath1 + "/" : "";
-            typeCombo += SubPath2 != null ? SubPath2 + "/" : "";
-            typeCombo += SubPath3 != null ? SubPath3 + "/" : "";
+            Debug.WriteLine(navManager.BaseUri.ToString());
 
-            Debug.WriteLine("typecombo-------- "+typeCombo);
-            
-            PaginatorCallback?.Invoke(SelectedPage, await Count(typeCombo));
+            IReadOnlyDictionary<string, object?> dict = new Dictionary<string, object?>();
+            string str = navManager.GetUriWithQueryParameters("/products/"+totalPath, dict);
+            navManager.NavigateTo(str);
 
-            
-            
 
-            await Task.Run(()=>GetCardData(typeCombo, tokenSource.Token), tokenSource.Token);
 
-            
+
+
+
+
+
+            totalNumberOfItems = await Count(totalPath);
+
+            await GetCardData();
             
 
             await base.OnParametersSetAsync();
         }
 
-        
 
-
-        private async Task GetCardData(string typeCombo, CancellationToken token)
+        private async Task GetCardData()
         {
+            
+
+            
+            
+
+            tokenSource?.Cancel();
+            tokenSource?.Dispose();
+            tokenSource = new CancellationTokenSource();
+            await Task.Run(() => GetCardData(totalPath, tokenSource.Token), tokenSource.Token);
+
+        }
+
+
+
+        private async Task GetCardData(string path, CancellationToken token)
+        {            
+
             Items.Clear();
-            await foreach (var v in cosmosDB.GetProducts<ProductCardDTO>(typeCombo, SearchWord != null ? SearchWord : "", ProductSorting.name, offset: SelectedPage * ItemsPerPage, limit: ItemsPerPage, token))
+            await foreach (var v in cosmosDB.GetProducts<ProductCardDTO>(path, SearchWord != null ? SearchWord : "", ProductSorting.name, offset: selectedPage * itemsPerPage, limit: itemsPerPage, token))
             {
                 if (!token.IsCancellationRequested)
                 {
@@ -148,6 +151,15 @@ namespace BlazorServerCrud1.Pages
         {
             tokenSource?.Cancel();
             tokenSource?.Dispose();
+            
+        }
+
+        public async Task OnSelectPage(int i)
+        {            
+            selectedPage = i;            
+            
+            
+            await GetCardData();
             
         }
     }
--- TestProject/ProductComponents.cs
+++ TestProject/ProductComponents.cs
@@ -10,21 +10,22 @@ namespace Test.ProductComponents
 
         [Fact]
         public void Test1()
-        {
-            
-
+        { 
             var cut = RenderComponent<ProductTitle>(parameters=>parameters.AddChildContent("hello world"));
-            
-
-            
             cut.Find("div").MarkupMatches("<div>hello world</div>");
-
-
         }
 
 
+        [Theory]
+        [InlineData("hello world")]
+        [InlineData("cruel world")]
+        public void TestTheory1(string str)
+        {
+            var cut = RenderComponent<ProductTitle>(parameters => parameters.AddChildContent(str));
+            cut.Find("div").MarkupMatches($"<div>{str}</div>");
+        }
 
-    
+
     }
 
 
